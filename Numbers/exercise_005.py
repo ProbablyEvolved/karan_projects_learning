757c8deef7695e61de291abca2159b4c7d516485
ProbablyEvolved <ujjwalakshith.m@gmail.com>
2022-05-08 13:13:23 +0530

    005 Done

--- Numbers/exercise_005.py
+++ Numbers/exercise_005.py
@@ -1,5 +1,5 @@
 # Next Prime Number
-#  Have the program find prime numbers until the user chooses to stop asking for the next one.
+# Have the program find prime numbers until the user chooses to stop asking for the next one.
 # https://github.com/karan/Projects
 # 005
 
